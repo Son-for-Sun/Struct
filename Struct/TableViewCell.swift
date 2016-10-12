474c25af77b23181a67cdd582aa6a676de37e766
Nother <yw529612700@163.com>
2016-10-12 18:03:09 +0800

    没事找谁

---
+++ Struct/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  Struct
+//
+//  Created by 太阳在线 on 16/10/12.
+//  Copyright © 2016年 太阳在线. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
